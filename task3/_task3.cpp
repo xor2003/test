de4d739dd69c85874eccd850dc0f903f3184444a
x0r <xorrezerv2@list.ru>
2018-11-29 04:07:31 +0300

    Optimize the solution

--- task3/_task3.cpp
+++ task3/_task3.cpp
@@ -40,7 +40,7 @@ struct Sorter
  {
 
     typename K::const_iterator k; // iterator pointing to data container value
-    typename V::const_iterator v; // iterator pointing to key container value
+    typename V::iterator v; // iterator pointing to key container value
 
     sort_proxy() {}
 
@@ -53,14 +53,14 @@ struct Sorter
     Sorter()
     {}
 
-    void run (const K& keys, const V& data, V& result_data)
+    void run (const K& keys, V& data)
     {
 
         std::vector<sort_proxy > proxies(keys.size());
 
         {
  	 size_t proxy_i=0;
-	 typename V::const_iterator v = data.begin(); // iterator to data container
+	 typename V::iterator v = data.begin(); // iterator to data container
          for (typename K::const_iterator k = keys.begin(); k != keys.end(); k++) // initialize sorting proxy container
          {
              proxies[proxy_i].k = k; // put key iterator to proxy container
@@ -72,13 +72,23 @@ struct Sorter
 
         std::sort(proxies.begin(), proxies.end(), F()); // sorting proxy container
 
-        result_data.resize(keys.size());
-	typename V::iterator res_i = result_data.begin(); // result container iterator
-        for (size_t j=0; j<proxies.size(); j++) // generating resulting data
+	V temp_data;
+      {
+        temp_data.resize(keys.size());
+	typename V::iterator res_i = temp_data.begin(); // temporary container iterator
+        for (size_t j=0; j<proxies.size(); j++)
         {
-            *res_i = *(proxies[j].v); // move data to result container
+            res_i->assign((std::string&&) *(proxies[j].v)); // move data to temp container
 	    res_i++;
         }
+      }
+	typename V::iterator res_o = data.begin(); // result container iterator
+        for (typename V::const_iterator res_i = temp_data.begin(); res_i != temp_data.end(); res_i++) // generating resulting data
+        {
+            res_o->assign((std::string&&) *(res_i)); // move data to result container
+	    res_o++;
+        }
+
     }
 
 };
@@ -96,12 +106,11 @@ int main (int argc, char *argv[])
     data_type data = {{ "a", "b", "c" }}; // first container with data
     const keys_type keys = {{ 4, 7, 1 }}; // second container with the key
 
-    data_type result_data;  // result container with data
     Sorter<keys_type, data_type> s;
-    s.run(keys, data, result_data);
+    s.run(keys, data);
 
     // print result to screen
-    std::copy(result_data.begin(), result_data.end(), std::ostream_iterator<std::string>( std::cout, " "));
+    std::copy(data.begin(), data.end(), std::ostream_iterator<std::string>( std::cout, " "));
     std::cout << std::endl;
 
     return 0;
